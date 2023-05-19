84985122a6dc687f0d6cd778ca5521c838a65690
sanazkiani <sanaz.kiani.2019@gmail.com>
2023-05-19 05:27:24 +0430

    add a main method for test

---
+++ newProject/skia/app/src/main/java/BiscuitClass.java
@@ -0,0 +1,12 @@
+public class BiscuitClass {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+        System.out.println("str.length()" + str.length());
+
+        String days[] = {"Sun", "Mon", "wed", "thu", "fri", "sat" };
+        System.out.println("days.length" + days.length);
+
+    }
+}
